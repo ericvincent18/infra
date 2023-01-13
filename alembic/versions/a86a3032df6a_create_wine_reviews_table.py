77c31c146eff444c2f253c0bb7539a4ea00c4bd6
ericvincent18 <evincent18@gmail.com>
2023-01-13 14:00:58 +1100

    alembic modifications for initial migration

--- alembic/versions/a86a3032df6a_create_wine_reviews_table.py
+++ alembic/versions/a86a3032df6a_create_wine_reviews_table.py
@@ -1,7 +1,7 @@
 """create wine_reviews table
 
 Revision ID: a86a3032df6a
-Revises: 
+Revises:
 Create Date: 2023-01-12 17:26:33.712788
 
 """
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = 'initial migration'
+revision = "initial migration"
 down_revision = None
 branch_labels = None
 depends_on = None
@@ -18,23 +18,23 @@ depends_on = None
 
 def upgrade() -> None:
     op.create_table(
-        'wine_reviews',
-        sa.Column('id', sa.Integer, primary_key=True),
-        sa.Column('points', sa.Integer),
-        sa.Column('title', sa.String(255)),
-        sa.Column('description', sa.String(255)),
-        sa.Column('taster_name', sa.String(255)),
-        sa.Column('taster_twitter_handle', sa.String(255)),
-        sa.Column('price', sa.Integer),
-        sa.Column('designation', sa.String(255)),
-        sa.Column('variety', sa.String(255)),
-        sa.Column('region_1', sa.String(255)),
-        sa.Column('region_2', sa.String(255)),
-        sa.Column('province', sa.String(255)),
-        sa.Column('country', sa.String(255)),
-        sa.Column('winery', sa.String(255))
+        "wine_reviews",
+        sa.Column("id", sa.Integer, primary_key=True),
+        sa.Column("points", sa.Integer),
+        sa.Column("title", sa.String(255)),
+        sa.Column("description", sa.String(255)),
+        sa.Column("taster_name", sa.String(255)),
+        sa.Column("taster_twitter_handle", sa.String(255)),
+        sa.Column("price", sa.Integer),
+        sa.Column("designation", sa.String(255)),
+        sa.Column("variety", sa.String(255)),
+        sa.Column("region_1", sa.String(255)),
+        sa.Column("region_2", sa.String(255)),
+        sa.Column("province", sa.String(255)),
+        sa.Column("country", sa.String(255)),
+        sa.Column("winery", sa.String(255)),
     )
 
 
 def downgrade() -> None:
-    op.drop_table('wine_reviews')
+    op.drop_table("wine_reviews")
