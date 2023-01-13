a8ef0e30d27a08415ebbb44fee61a5e5217697b3
ericvincent18 <evincent18@gmail.com>
2023-01-13 14:03:04 +1100

    main service and data processing added

---
+++ app/sql/process_data.py
@@ -0,0 +1,51 @@
+import sys
+import logging
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import pandas as pd
+
+sys.path.insert(1, "/Users/ericvincent/infra/infra/app/models")
+from reviews import Wine
+
+sys.path.insert(1, "/Users/ericvincent/infra/infra/app/config")
+from env_config import config
+
+class initSession:
+    def __init__(self):
+
+        self.engine = create_engine(config.db_connection_string)
+        self.Session = sessionmaker(bind=self.engine)
+        self.session = self.Session()
+
+# Connect to the database
+session = initSession()
+
+
+class dataProcessing:
+    def __init__(self, df: pd.DataFrame):
+        self.df = pd.DataFrame(df)
+
+    def formatDf(self):
+        self.df = self.df.fillna(0)
+        return self.df
+
+    def insert_data(self):
+
+        for index, row in self.df.iterrows():
+            wine = Wine(
+                points=row["points"],
+                title=row["title"],
+                description=row["description"],
+                taster_name=row["taster_name"],
+                taster_twitter_handle=row["taster_twitter_handle"],
+                price=row["price"],
+                designation=row["designation"],
+                variety=row["variety"],
+                region_1=row["region_1"],
+                region_2=row["region_2"],
+                province=row["province"],
+                country=row["country"],
+                winery=row["winery"],
+            )
+            session.session.add(wine)
+        session.session.commit()
--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 import pika
 import time
-import pandas as pd
 import json
-from app.models.reviews import Wine
 
 connection = pika.BlockingConnection(pika.ConnectionParameters(host="127.0.0.1"))
-time.sleep(10)
+time.sleep(2)
 channel = connection.channel()
 
 channel.queue_declare(queue="task_queue", durable=True)
@@ -23,18 +21,18 @@ if method_frame:
 channel.basic_qos(prefetch_count=1)
 channel.basic_consume(queue="task_queue", on_message_callback=callback)
 res_dict = json.loads(body.decode("utf-8"))
-df = pd.DataFrame(res_dict)
 
-a = 1
-# TODO Dataframe function 
+from app.sql.process_data import initSession, dataProcessing
+
+df = dataProcessing(res_dict)
+df.formatDf()
+df.insert_data()
+
+
 # TODO Logger
 # TODO Save to db -- delete previously submitted rows
 # TODO deployments
 # TODO full update of readme
 # TODO unlimited description limit
-# TODO configs + hide secrets
 # TODO update requirements .txt
 # TODO  handle initial migration with docker / k8s
-
-
-
