5424148416740a37f038247f2306dfe0542ad23d
ericvincent18 <evincent18@gmail.com>
2023-01-19 17:07:45 +1100

    created dev folder

--- app/messaging/new_task.py
+++ app/messaging/new_task.py
@@ -1,20 +1,23 @@
 import pika
+import logging
+import time
+import socket
+socket.gethostbyname("")
 
-connection = pika.BlockingConnection(
-    pika.ConnectionParameters(host="localhost", port=15672)
-)
+parameters = pika.URLParameters('amqp://user:pass@localhost:5672/%2F')
+
+connection = pika.BlockingConnection(parameters)
+
 channel = connection.channel()
 
-channel.queue_declare(queue="task_queue", durable=True)
 with open("/Users/ericvincent/Desktop/winemag-data-130k-v2.json", "r") as read_file:
     f = read_file.read()
+    message = f
 
-message = f
-channel.basic_publish(
-    exchange="",
-    routing_key="task_queue",
-    body=message,
-    properties=pika.BasicProperties(delivery_mode=pika.spec.PERSISTENT_DELIVERY_MODE),
-)
+channel.basic_publish('',
+                      'test_routing_key',
+                      message,
+                      pika.BasicProperties(content_type='text/plain',
+                                           delivery_mode=pika.DeliveryMode.Transient))
 
 connection.close()
